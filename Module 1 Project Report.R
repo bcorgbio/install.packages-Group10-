22968a0391c3dade95d67c269cbb9df440b71870
lex lombardi <lexilombardi@lexs-mbp.bc.edu>
2022-09-09 14:26:19 -0400

    9/9/22 commit

--- Module 1 Project Report.R
+++ Module 1 Project Report.R
@@ -1,30 +1,46 @@
 library(ggplot2)
+
 library(tidyverse)
+
+setwd("~/Downloads/ABDLabs/Group_10_Repo/DataForLabs/scales")
+
 dat<-read.csv("scales.csv")
+
 dim(dat)
+
 sapply(dat,class)
+
 dat$species <- as.factor(dat$species)
+
 species <- levels(dat$species)
+
 species.n<- dat %>%
   group_by(species) %>%
   summarise(n = n())
+
 species.n
+
 dat %>% 
   count(species,specimen) %>%
   print() %>%
   count(species,name = "n.specimens")
+
 for(i in species){
   p <- dat %>%
     filter(species==i)%>%
     ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
   print(p)
 }
-pdf("species.quadrant.alexa.pdf")
+
+pdf("species.quadrant.pdf")
+
 for(i in species){
   p <- dat %>%
     filter(species==i)%>%
     ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
   print(p)
 }
+
 dev.off()
+
 list.files(pattern=".pdf")
--- test.R
+++
@@ -1,4 +0,0 @@
-setwd("~/Downloads/ABDLabs/Group_10_Repo/team_repo/Group_10_Repo")
-v<-c(1,2,3)
-hello
-test update
