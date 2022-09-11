56423fb1f92404a27eb8f92ae74cdbe1af431286
jklos123 <112418217+jklos123@users.noreply.github.com>
2022-09-11 17:51:29 -0400

    Add files via upload

---
+++ Module 1 Project - Kloster.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(tidyverse)
+setwd("~jackkloster/Desktop/install.packages-Group10-/scales")
+dat <- read.csv("scales.csv")
+dim(dat)
+sapply(dat,class)
+species.n<- dat %>%
+  group_by(species) %>%
+  summarise(n = n())
+species.n
+dat %>% 
+  count(species,specimen) %>%
+   print() %>%
+  count(species,name = "n.specimens")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+pdf("species.quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
+list.files(pattern=".pdf")
