ab51f4f44912777dd20bb67640c4e71b2e7e79fb
Christopher Kenaley <cpkenaley@gmail.com>
2022-09-24 06:44:45 -0400

    proj2 feedback

---
+++ JK Project #2_comments.R
@@ -0,0 +1,108 @@
+#setup library/data
+library(ggplot2)
+library(tidyverse)
+library(features)
+
+pseed <- read_csv("pseed.fin.amps.csv")
+pseed.bl <- read_csv("pseed.lengths.csv")
+speeds <- read_csv("pseed.calibration.csv")
+
+pseed2 <- pseed %>%
+  left_join(speeds,by=c("speed"="vol")) %>%
+  left_join(pseed.bl,by="fish")
+  
+  pseed2 <- pseed2 %>%
+  mutate(bl.s=cm.s/bl)
+  
+  pseed.wide <- pseed2 %>%
+  select(-amp)%>%
+  pivot_wider(names_from = fin,values_from = amp.bl) %>%
+  mutate(amp.sum=L+R)
+  
+  #CPK: This ^^ could have been done a little more concisely. . . .
+  
+  pseed.wide <- pseed%>%
+    left_join(speeds,by=c("speed"="vol"))%>%
+    left_join(pseed.bl,by="fish")%>%
+    mutate(bl.s=cm.s/bl)%>%
+    select(-amp)%>%
+    pivot_wider(names_from = fin,values_from = amp.bl) %>%
+    mutate(amp.sum=L+R)%>%
+    print() 
+  
+  #mean-max amp
+  find.peaks <- function(x,y,mult=100){
+    f <- fget(features(x = x,y=y*mult))[2:3]%>% 
+      as_tibble() %>% 
+      filter(curvature<0)%>% 
+      mutate(peaks=round(crit.pts,0))
+    return(f$peaks) 
+  }
+
+
+
+  pseed.max <- pseed.wide%>%
+    group_by(fish,speed)%>%
+    mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
+    filter(peak==T)  
+
+pseed.sum.max <- pseed.wide%>%
+  group_by(date,fish,bl.s)%>%
+  mutate(peak=frame %in% find.peaks(frame,amp.sum))%>%
+  filter(peak==T)%>%
+  mutate(max.amp.sum = T, amp.sum.mean = mean(max.amp.sum))
+
+#CPK: Yes, for this  we needed to group by date bc cycles refer to each experiment, not across them. But, why is so much of this ^ being repeated? [-1]. Not sure why you're computing mean on single rows.
+
+pseed.sum.max<- pseed.max %>%
+  group_by(fish, speed,bl.s) %>%
+  summarize(amp.sum.mean=mean(amp.sum),amp.sum.se = stderror(amp.sum)) 
+
+#CPK:^^Good on ya for thinking to combine the mean and se operations, however, this throws and error bc you haven't yest defined the functon stederror. [-1]. I addes "bl.s" to group_by so we could merge with the met data.
+
+
+# custom function to compute SE // error bars
+stderror <- function(x) sd(x)/sqrt(length(x))
+
+pseed.sum.max
+
+pseed.sum.max %>% 
+  ggplot(aes(x=speed,y=amp.sum.mean,col=fish))+
+  geom_point()+geom_smooth(method="lm")+
+  geom_errorbar(aes(ymin=amp.sum.mean-amp.sum.se, ymax=amp.sum.mean+amp.sum.se), width=0.5, colour="black")+
+  theme_classic()
+
+#CPK: Why not col the error bars by fish, too?
+  
+#Merge with new pseed.sum.max tibble 
+pseed.met.rate <- read_csv("pseed.met.rate.csv")
+#pseed.sum.max$bl.s <- as.numeric(as.character(pseed.sum.max$bl.s))
+#pseed.met.rate$bl.s <- as.numeric(as.character(pseed.met.rate$bl.s))
+
+#CPK: Commenting this ^^ out bc it's unneeded and prevets tibbles from merging. 
+pseed.met.rate2 <- pseed.met.rate %>% 
+  select(fish,bl.s,met.rate) %>% 
+  print()
+pseed.sum.max <- pseed.sum.max%>%
+  left_join(pseed.met.rate2,by=c("fish"="fish","bl.s"="bl.s"))%>%
+  print()
+pseed.sum.max.met <- pseed.sum.max%>%
+  left_join(pseed.met.rate2,by=c("fish"="fish","bl.s"="bl.s"))%>%
+  print()
+summarize(amp.met.rate=mean(met.rate))
+
+#CPK: This^^^was tough bc you didn't preserve the bl.s column in pseed.sum.max. But, could have been this easy if you had.
+
+pseed.sum.max%>%
+  left_join(pseed.met.rate%>%
+              group_by(fish,bl.s)%>%
+              summarize(amp.met.rate=mean(met.rate))) %>% 
+  ggplot(aes(x=amp.sum.mean, y=amp.met.rate, colour=fish)) + geom_point()
+
+#ggplot to plot metabolic power vs. mean max
+pseed.sum.max %>% ggplot(pseed.sum.max.met, aes(x=amp.sum.mean, y=met.rate, colour=fish)) + geom_point()
+
+#CPK: Best to use the pipe^
+
+#CPK: Pretty good work. Getting there! Just need to get a better grasp on when to you to pipe to become more concise and address the project propmpts
+
