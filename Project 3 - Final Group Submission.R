be6d541cc26080631db5ce348072cb7e5cbf8455
Christopher Peters <petersuo@bc.edu>
2022-09-27 23:39:01 -0400

    Module 3 Final Group Submission

--- Project 3 - Final Group Submission.R
+++ Project 3 - Final Group Submission.R
@@ -22,29 +22,29 @@ anole.log<-anole %>%
   left_join(anole.eco)%>%
   filter(!Ecomorph%in%c("U","CH"))%>%
   na.omit()%>%
-  mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)
+mutate_at(c("SVL", "HTotal","PH","ArbPD"),log)
 
 
 #Step 2 - creating linear moddles for perch height and diameter
 
 #For perch height
-anole.log.ph.lm<-lm(HTotal~SVL+PH, anole.log)
+anole.log.PH.LM<-lm(HTotal~SVL+PH, anole.log)
 
 #For perch diameter
-anole.log.pd.lm<-lm(HTotal~SVL+ArbPD, anole.log)
+anole.log.PD.LM<-lm(HTotal~SVL+ArbPD, anole.log)
 
 
 #Step 3 - How perch height and diameter affect Hindlimb relation
 
 #Adding residual
 anole.log<-anole.log %>%
-  mutate(res.ph = residuals(anole.log.ph.lm), 
-  res.pd = residuals(anole.log.pd.lm))
+  mutate(resPH = residuals(anole.log.PH.LM), 
+  resPD = residuals(anole.log.PD.LM))
 
 #Perch height and diameter
 anole.log%>%
-  dplyr::select(Ecomorph2,res.ph,res.pd)%>%
-  pivot_longer(cols=c("res.ph","res.pd"))%>%
+  dplyr::select(Ecomorph2,resPH,resPD)%>%
+  pivot_longer(cols=c("resPH","resPD"))%>%
   print%>%
   ggplot(aes(x=Ecomorph2,y=value)) +
   geom_boxplot() +
@@ -54,27 +54,28 @@ anole.log%>%
 
 #Step 4 - Using BM model and model for phylogenetic least squares
 
-anole.tree<-read.tree("anole.tre")
+#Reading anole.tre
+anoletree<-read.tree("anole.tre")
 
 #Model for hindlimb relationship and perch height
-pgls.BM.ph <- gls(HTotal~SVL + PH, 
-  correlation = corBrownian(1, phy = anole.tree, form =~Species), 
-  data= anole.log, method = 'ML')
+pgls.BM.PH <- gls(HTotal~SVL + PH, 
+correlation = corBrownian(1, phy = anoletree, form =~Species), 
+data= anole.log, method = 'ML')
 
 #Model for hindlimb relationship and perch diameter
-pgls.BM.pd <- gls(HTotal~SVL + ArbPD, 
-  correlation = corBrownian(1, phy = anole.tree, form =~Species), 
-  data= anole.log, method = 'ML')
+pgls.BM.PD <- gls(HTotal~SVL + ArbPD, 
+correlation = corBrownian(1, phy = anoletree, form =~Species), 
+data= anole.log, method = 'ML')
 
 #Model for hindlimb relationship, perch height, and perch diameter
-pgls.BM.ph.pd <- gls(HTotal~SVL + PH + ArbPD, 
-  correlation = corBrownian(1, phy = anole.tree, form =~Species), 
-  data= anole.log, method = 'ML')
+pgls.BM.PH.PD <- gls(HTotal~SVL + PH + ArbPD, 
+correlation = corBrownian(1, phy = anoletree, form =~Species), 
+data= anole.log, method = 'ML')
 
 
 #Assesment of the fit of the models 
-anole.phylo.aic <- MuMIn::AICc(pgls.BM.ph, pgls.BM.pd, pgls.BM.ph.pd)
-aicw(anole.phylo.aic$AICc)
+anole.Phylo.AIC <- MuMIn::AICc(pgls.BM.PH, pgls.BM.PD, pgls.BM.PH.PD)
+aicw(anole.Phylo.AIC$AICc)
 
 # model     fit        delta        w
 #   1   -64.77956   10.746149 0.003241168
@@ -91,18 +92,17 @@ aicw(anole.phylo.aic$AICc)
 
 
 #Step 6 - Creating our own plot
-#Mutating anole.log to include best phylo plot
-anole.log <- anole.log %>%
-  mutate(phylo.res = residuals(pgls.BM.ph.pd))
+#Mutating anole.log to include best phylo plot and creating facet grid
 
-#Creating Facet grid of variables 
-anole.log %>% 
-  dplyr::select(Ecomorph2,res.ph,res.pd,phylo.res) %>%
-  pivot_longer(cols = c("res.ph","res.pd", "phylo.res"))%>%
+anole.log <- anole.log %>% 
+  mutate(phylo.residual=residuals(pgls.BM.PH.PD))
+  
+anole.log%>%
+  dplyr::select(phylo.residual,Ecomorph2, resPH,resPD)%>%
+  pivot_longer(cols=c("resPH","resPD"))%>%
   print%>%
   ggplot(aes(x = Ecomorph2,y = value)) + 
   geom_boxplot() +
   stat_summary(fun=mean, geom="point", size=3)+
   facet_grid(name~.,scales = "free_y") + ylab("residual")
-
 
